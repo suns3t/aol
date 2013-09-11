608e6847f3acf93deaef0ef2d3fa8b96fcf1c28e
Matt Johnson <mdj2@pdx.edu>
2013-09-11 09:48:15 -0700

    Refactor tests; beef up test_thumbnail_url
    The test_thumbnail_url test needed to make sure the thumbnail wasn't
    regenerated on subsequent calls.

--- aol/models/tests/lakes.py
+++ aol/models/tests/lakes.py
@@ -1,6 +1,7 @@
 import os
 from django.test import TestCase
 from django.contrib.gis.geos import GEOSGeometry
+from django.conf import settings as SETTINGS
 from ..models import Lake, FishingZone, County, HUC6, LakeCounty, Photo
 
 class LakeTest(TestCase):
@@ -18,22 +19,53 @@ class LakeTest(TestCase):
             self.assertTrue(lake.counties, "Clark, Washington")
 
     def test_watershed_tile_url(self):
+        # just make sure the URL has a bbox set in it
         lake = Lake.objects.get(title="Matt Lake")
         url = lake.watershed_tile_url
         self.assertTrue("?bbox=-295,-295,345,340" in url)
 
     def test_basin_tile_url(self):
+        # just make sure the URL has a bbox set in it
         lake = Lake.objects.get(title="Matt Lake")
         url = lake.basin_tile_url
         self.assertTrue("?bbox=-995,-995,1045,1040" in url)
 
     def test_url(self):
-        photo = next(iter(Photo.objects.all()))
+        # make sure the URL path is valid
+        photo = Photo.objects.get(pk=1)
         self.assertEqual(photo.url, "/media/photos/test.jpg")
 
     def test_thumbnail_url(self):
-        photo = next(iter(Photo.objects.all()))
-        self.assertEqual(photo.thumbnail_url, "/media/photos/thumbnail-test.jpg")
-        # this method should have the side effect of generating a thumbnail, so delete it
-        os.remove(photo._thumbnail_path)
+        photo = Photo.objects.get(pk=1)
 
+        # delete the thumbnail file
+        thumb_path = SETTINGS.ROOT('media', 'photos', 'thumbnail-test.jpg')
+        thumb_url = "/media/photos/thumbnail-test.jpg"
+        try:
+            os.remove(thumb_path)
+        except OSError:
+            pass
+
+        # this should generate a thumbnail
+        self.assertEqual(photo.thumbnail_url, thumb_url)
+        # make sure the thumbnail file actually exists
+        self.assertTrue(os.path.exists(thumb_path))
+
+        # now we want to make sure subsequent calls to the thumbnail_url do not
+        # recreate the image (since that would be expensive)
+
+        # overwrite the file
+        flag_text = "this should not be overwritten!"
+        with open(thumb_path, 'w') as f:
+            f.write(flag_text)
+        # try getting the thumbnail URL again
+        self.assertEqual(photo.thumbnail_url, thumb_url)
+        # make sure it didn't recreate the thumbnail
+        with open(thumb_path) as f:
+            self.assertEqual(f.read(), flag_text)
+
+        # delete the thumbnail file
+        try:
+            os.remove(thumb_path)
+        except OSError:
+            pass
