bb5db433d9adc1592381f05011616ceab51e581c
Matt Johnson <mdj2@pdx.edu>
2013-12-13 16:52:26 -0800

    Implement facilities; model; kml generating view; JS

--- aol/maps/views.py
+++ aol/maps/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from aol.lakes.models import Lake
+from aol.lakes.models import Lake, Photo, Document, Facility
 
 def home(request):
     """Displays the interactive map"""
@@ -17,4 +17,23 @@ def lakes(request):
     lakes = Lake.objects.to_kml(scale=scale, bbox=bbox)
     return render(request, "maps/lakes.kml", {
         "lakes": lakes,
+    })
+
+def facilities(request):
+    """Return the KML for the facilities"""
+    bbox = map(float, request.GET['bbox_limited'].split(","))
+
+    facilities = Facility.objects.to_kml(bbox=bbox)
+    return render(request, "maps/facilities.kml", {
+        "rows": facilities,
+    })
+
+def panel(request, reachcode):
+    lake = get_object_or_404(Lake, reachcode=reachcode)
+    photos = Photo.objects.filter(lake=lake)
+    documents = Document.objects.filter(lake=lake)
+    return render(request, "maps/panel.html", {
+        "lake": lake,
+        "photos": photos,
+        "documents": documents,
     })
